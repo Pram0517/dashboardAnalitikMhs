988bc66d3fe9beda33511049d6df147e3e8b2a6e
Pram0517 <pramudinhaqcuh@gmail.com>
2026-07-14 19:54:23 +0700

    delete dosen

--- Dashboard-Analitik-Mhs/src/data/dosenData.js
+++ Dashboard-Analitik-Mhs/src/data/dosenData.js
@@ -7,6 +7,5 @@ export const dosenData = [
   { id: "D06", nama: "Farid Suryanto, S.Pd., MT.", nip: "198504042009121006", kuota: 10, bidang_keahlian: "Pengembangan Web" },
   { id: "D07", nama: "Dr. Mursid Wahyu Hananto, S.Si., M.Kom.", nip: "197805052005011007", kuota: 10, bidang_keahlian: "Sistem Terdistribusi" },
   { id: "D08", nama: "Tawar, S.Si., M.Kom.", nip: "198106062005011008", kuota: 10, bidang_keahlian: "Kriptografi" },
-  { id: "D09", nama: "Prof. Dr. Ir. Imam Riadi, M.Kom.", nip: "197007072000031009", kuota: 10, bidang_keahlian: "Forensika Digital" },
   { id: "D10", nama: "Azty Acbarrifha Nour, S.T., M.Eng.", nip: "198608082010122010", kuota: 10, bidang_keahlian: "Internet of Things" }
 ];
